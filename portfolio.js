6be8d9c55852a23d36fe48625185ba4e73e3cfe6
taksh.doria <60806278+taksh-doria@users.noreply.github.com>
2021-12-23 12:13:11 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -122,8 +122,8 @@ export const educationInfo = [
 		subHeader: "Computer Engineering",
 		duration: "July 2018 - May 2022",
 		descBullets: [
-			"CPI 8.14(till 6th sem)",
-			"currently in 7th semester",
+			"CPI 8.23(till 7th sem)",
+			"currently in 8th semester",
 		],
 	},
 	{
